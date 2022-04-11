dc98ad4face6f5fac6b91a54803b4a356edcbbb5
mathexp <99506357+mathexp@users.noreply.github.com>
2022-04-11 15:36:08 -0700

    Update 4958.py

--- 01/4958.py
+++ 01/4958.py
@@ -1,11 +1,23 @@
-def solve():
+PROBLEM = "10 6-sided dice are rolled. What is the probability that exactly three of the dice show a 1? Express your answer as a decimal rounded to the nearest thousandth."
+SOLUTION = 0.155
+
+def probability_three_dice_show_one(number_of_dice, number_of_sides):
   """
-  This function solves the following problem and returns the answer.
-  Ten 6-sided dice are rolled. What is the probability that exactly three of the dice show a 1? Express your answer as a decimal rounded to the nearest thousandth.
+  Given number_of_dice dice, each with number_of_sides sides, returns the
+  probability that exactly three of the dice show a 1, rounded to the nearest
+  thousandth.
   """
   from itertools import combinations
 
-  number_of_sides = 6
-  probability_three_dice_show_one = len(list(combinations(range(10), 3)))
+  probability_three_dice_show_one = len(list(combinations(range(number_of_dice), 3)))
   probability_of_any_one_outcome = (1. / number_of_sides)**3 * (1 - (1. / number_of_sides))**7
   return round(probability_of_any_one_outcome * probability_three_dice_show_one, 3)
+
+def problem(number_of_dice, number_of_sides):
+  return f"{number_of_dice} {number_of_sides}-sided dice are rolled. What is the probability that exactly three of the dice show a 1? Express your answer as a decimal rounded to the nearest thousandth."
+
+def test():
+  assert problem(10, 6) == PROBLEM
+  assert probability_three_dice_show_one(10, 6) == SOLUTION
+
+test()
