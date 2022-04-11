1633fdc5a37e4c64e351a182db0ebd7daa09bc02
mathexp <99506357+mathexp@users.noreply.github.com>
2022-04-11 15:13:41 -0700

    Update 2729.py

--- 01/2729.py
+++ 01/2729.py
@@ -1,8 +1,17 @@
-def solve():
-  """
-  What is $11111111_2+111111_2$? Write your answer in base $10$.
-  """
-  summand_1 = "11111111"
-  summand_2 = "111111"
+"""
+What is $11111111_2+111111_2$? Write your answer in base $10$.
+"""
 
+def sum_two_binary_numbers_to_base_ten(summand_1, summand_2):
+  """
+  Returns the sum of two binary numbers, summand1 and summand2 in base 10
+  """
   return int(summand_1, 2) + int(summand_2, 2)
+
+
+def problem(summand_1, summand_2):
+  return f"What is ${summand_1}_2+{summand_2}_2$? Write your answer in base $10$."
+  
+
+def test():
+  assert sum_two_binary_numbers_to_base_ten("11111111", "111111") == 318
