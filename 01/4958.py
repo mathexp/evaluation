2ed3bf2e5f6924dd90bf02d8a5786bab03958b31
mathexp <99506357+mathexp@users.noreply.github.com>
2022-04-11 17:06:43 -0700

    Update 4958.py

--- 01/4958.py
+++ 01/4958.py
@@ -1,7 +1,7 @@
 PROBLEM = "10 6-sided dice are rolled. What is the probability that exactly three of the dice show a 1? Express your answer as a decimal rounded to the nearest thousandth."
 SOLUTION = 0.155
 
-def probability_three_dice_show_one(number_of_dice, number_of_sides):
+def probability_three_dice_show_one(number_of_dice: int, number_of_sides: int):
   """
   Given number_of_dice dice, each with number_of_sides sides, returns the
   probability that exactly three of the dice show a 1, rounded to the nearest
@@ -13,7 +13,7 @@ def probability_three_dice_show_one(number_of_dice, number_of_sides):
   probability_of_any_one_outcome = (1. / number_of_sides)**3 * (1 - (1. / number_of_sides))**(number_of_dice - 3)
   return round(probability_of_any_one_outcome * probability_three_dice_show_one, 3)
 
-def problem(number_of_dice, number_of_sides):
+def problem(number_of_dice: int, number_of_sides: int):
   return f"{number_of_dice} {number_of_sides}-sided dice are rolled. What is the probability that exactly three of the dice show a 1? Express your answer as a decimal rounded to the nearest thousandth."
 
 def test():
